8714dee3b3bf611b2aa13734385e2b0de60ba09b
JD Hiudt & Maureen Riley <pair+jd-hiudt.moriley5@devbootcamp.com>
2017-03-08 16:01:27 -0600

    rails config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AListForOdin::Application.config.secret_key_base = 'dcc2099ad793b1e77af8ee812e0751ab525241de64e0badba5ecc13c087e3cd1096ea0420bb525d4d1fbfe6335278ab4feb55cfca6ed1a01dab5e0072710acd8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AListForOdin::Application.config.session_store :cookie_store, key: '_a-list-for-odin_session'
