46ba9578640d2d651d827ad493c17eb0b821ff0a
Chase Wessman & Maureen Riley <pair+cjwess.moriley5@devbootcamp.com>
2017-03-09 16:10:56 -0600

    remove possibility of duplicate entries to profile page

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -1,10 +1,12 @@
 class OwnershipsController < ApplicationController
 
   def create
-    @ownership = Ownership.new(owner_id: params[:owner_id], game_id: params[:game_id])
-
-    if @ownership.save
-      redirect_to "/users/#{current_user.id}"
+    @game = Game.find_by(id: params[:game_id])
+    if current_user && @game.ownerships.where("owner_id = #{current_user.id}").count == 0
+      @ownership = Ownership.new(owner_id: params[:owner_id], game_id: params[:game_id])
+      if @ownership.save
+        redirect_to "/users/#{current_user.id}"
+      end
     else
       redirect_to "/"
     end
