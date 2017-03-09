0e92558b0dd9543fcac8125f953e3e1767916b24
Al Musavi & Chase Wessman <pair+almusavi.cjwess@devbootcamp.com>
2017-03-09 15:22:22 -0600

    wrote tests for model: user, ownership, game_ratings, relationships

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,17 @@ describe User do
   describe "validates" do
     it "has a valid username" do
       user.valid?
-      expect(user.errors[:username]).not_to be_empty
+      expect(user.errors[:username]).to be_empty
+    end
+    it "has a valid email" do
+      user.valid?
+      expect(user.errors[:email]).to be_empty
+    end
+    it "has a valid username" do
+      user.valid?
+      expect(user.errors[:password]).to be_empty
     end
   end
 end
+
+
